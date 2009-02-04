38c84ffbbdd7e6940c25f008cc74ecfa35fa8f5c
Sergei Tulentsev <sergei.tulentsev@gmail.com>
2009-02-04 17:01:25 -0200

    Add core wordpress files. Add Russian localization. Add facebox script.

---
+++ wp-content/languages/ru_RU.php
@@ -0,0 +1,31 @@
+<?php
+
+// делает меню на 35px шире
+function ru_extend_menu() { ?>
+	
+<style type="text/css">
+	#adminmenu {
+		width: 185px; /* default 145px + 10px */
+		margin-left: -200px; /* default 160px + 10px */
+	}
+	#wpbody {
+		margin-left: 215px; /* default 175px + 10px */
+	}
+	* html #adminmenu { /* for IE6 only */
+		margin-left: -115px; /* default 80px + 5px */
+	}
+</style>
+<?php
+}
+
+function change_update_url($options) {
+        foreach ( $options->updates as $key => $value ) {
+                $options->updates[$key] = (object)
+str_replace('http://ru.wordpress.org/',
+'http://lecactus.ru/download/', (array) $value); //здесь можно вписать и резервный путь http://wp2.lecactus.ru/download/
+        }
+        return $options;
+}
+add_filter('pre_update_option_update_core', 'change_update_url');
+add_action('admin_head', 'ru_extend_menu');
+?>
