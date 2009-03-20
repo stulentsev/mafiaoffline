d274bf1a72d3a87f764f5894d28028bc835a4a7a
Sergei Tulentsev <sergei.tulentsev@gmail.com>
2009-03-20 09:20:30 -0300

    Translate some sidebar headers

--- wp-content/themes/default/sidebar.php
+++ wp-content/themes/default/sidebar.php
@@ -49,15 +49,15 @@
 
 			</li> <?php }?>
 
-			<?php wp_list_pages('title_li=<h2>Pages</h2>' ); ?>
+			<?php wp_list_pages('title_li=<h2>О нас</h2>' ); ?>
 
-			<li><h2>Archives</h2>
+			<li><h2>Архив новостей</h2>
 				<ul>
 				<?php wp_get_archives('type=monthly'); ?>
 				</ul>
 			</li>
 
-			<?php wp_list_categories('show_count=1&title_li=<h2>Categories</h2>'); ?>
+			<?php wp_list_categories('show_count=1&title_li=<h2>Рубрики</h2>'); ?>
 
 			<?php /* If this is the frontpage */ if ( is_home() || is_page() ) { ?>
 				<?php wp_list_bookmarks(); ?>
